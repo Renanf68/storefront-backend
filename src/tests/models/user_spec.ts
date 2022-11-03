3814474e3dd9b453f886481b8f22815acd086485
Renan Costa <renancostam@gmail.com>
2022-11-03 18:57:55 -0300

    User model tests completed

--- src/tests/models/user_spec.ts
+++ src/tests/models/user_spec.ts
@@ -2,6 +2,13 @@ import { User, UserStore } from "../../models/user";
 
 const store = new UserStore();
 
+const testUser = {
+  username: "fanning",
+  first_name: "Mick",
+  last_name: "Fanning",
+  password: "AsasdG30f",
+} as User;
+
 describe("User Model", () => {
   it("should have an index method", () => {
     expect(store.index).toBeDefined();
@@ -16,25 +23,24 @@ describe("User Model", () => {
     const result = await store.index();
     expect(result).toEqual([]);
   });
-  xit("create method should return created user", async () => {
-    const userData = {
-      username: "fanning",
-      first_name: "Mick",
-      last_name: "Fanning",
-      password: "AsasdG30f",
-    } as User;
-    const result = await store.create(userData);
+  it("create method should return created user", async () => {
+    const result = await store.create(testUser);
     expect(result.username).toEqual("fanning");
   });
-  xit("create method should return error when user already exists", async () => {
-    const userData = {
-      username: "fanning",
-      first_name: "Mick",
-      last_name: "Fanning",
-      password: "AsasdG30f",
-    } as User;
-    await expectAsync(store.create(userData)).toBeRejectedWithError(
+  it("create method should return error when user already exists", async () => {
+    await expectAsync(store.create(testUser)).toBeRejectedWithError(
       "User fanning already exists."
     );
+  });
+  it("authentication method should return authenticated user", async () => {
+    const user = await store.authentication(
+      testUser.username,
+      testUser.password!
+    );
+    expect(user?.username).toEqual(testUser.username);
+  });
+  it("delete method should return deleted user", async () => {
+    const user = await store.delete("1");
+    expect(user.id.toString()).toEqual("1");
   });
 });
